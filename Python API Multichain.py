e5ee830aefac1ad06b35646055b15ac00cc3ef81
Fahad Sultan <fahadsultan1998@gmail.com>
2022-08-07 18:43:47 +0500

    Routes Changes

--- Python API Multichain.py
+++ Python API Multichain.py
@@ -1,27 +1,25 @@
-
 import requests
 import json
 import urllib.request
 import getpass
 import os
 import ast
-from flask import Flask, jsonify, request 
+import pickle
+import time
 import subprocess
+from flask import Flask, jsonify, request 
+from keyboard import press
 
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 chainname = ""
 port=4447
-host="127.0.0.1"
-
+host="127.0.0.1" 
 peeraddress1=""
 
-from keyboard import press
-import pickle
-import time
 
-
+# This route will initilalize  a blockchain Network
 @app.route('/createblockchain', methods=['POST'])
 def createblockchain():
     n = request.args.get('chain')    
@@ -32,25 +30,26 @@ def createblockchain():
 
 
 
-
+# This route will return the host Server address
 @app.route('/getpeeraddress',methods=['POST'])
 def getpeeraddress():
-    moizip="192.168.0.107"
+    peerIP2="192.168.0.107"
     localchain = request.args.get('local')
     os.system("start cmd /k "+"cd /d E:\Multichain")
     os.chdir(r"E:\Multichain")
-    addressOutput = os.popen("multichaind "+localchain+"@"+moizip+":6465").read()
+    addressOutput = os.popen("multichaind "+localchain+"@"+peerIP2+":6465").read()
     y = addressOutput.splitlines()
     print(y[4])
     return y[4];
     
+# This route will grant Permission to the peer    
 @app.route('/grantPermission',methods=['POST'])
 def grantPermission():
     commandId = request.args.get('ID')
     os.chdir(r"E:\Multichain")
     os.chdir(commandId)
 
-
+# This route will create a stream to save the data on Blockchain    
 @app.route('/CreateStream',methods=['POST'])
 def CreateStream():
     chainname = request.args.get('chain')
@@ -65,6 +64,7 @@ def CreateStream():
     jsonString = os.popen("multichain-cli "+chainname+" liststreamkeyitems medicine "+id+" true 1").read()
     return jsonString
 
+# This route will get a stream for first  peer   
 @app.route('/getSecondStream1',methods=['POST'])
 def getSecondStream1():
     chainname = request.args.get('chain')
@@ -95,6 +95,7 @@ def getSecondStream1():
     os.system("multichain-cli "+chainname+" publish medicine "+id+" "+hexPushData)
     return r
 
+# This route will get a stream for second  peer  
 @app.route('/getSecondStream2',methods=['POST'])
 def getSecondStream2():
     chainname = request.args.get('chain')
@@ -122,6 +123,7 @@ def getSecondStream2():
     os.system("multichain-cli "+chainname+" publish medicine "+id+" "+hexPushData)
     return r
 
+# This route will get a stream for third  peer
 @app.route('/getSecondStream3',methods=['POST'])
 def getSecondStream3():
     chainname = request.args.get('chain')
@@ -152,6 +154,7 @@ def getSecondStream3():
     return r
    
 
+# This route will help to publish data on network   
 @app.route('/PushSecondStream',methods=['POST'])
 def PushSecondStream():
     chainname = request.args.get('chain')
@@ -178,6 +181,7 @@ def PushSecondStream():
     return r
     
 
+# This route will Initialize Blockchain Network   
 @app.route('/InitializeBlockchain', methods=['POST'])
 def InitializeBlockchain():
     x = request.args.get('chain')
